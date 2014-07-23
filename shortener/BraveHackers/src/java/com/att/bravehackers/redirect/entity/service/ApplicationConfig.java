214400831610be2d17ebcf9c29ae104cf052bc96
ML5174 <ML5174@GACDTL03ML5174.ITServices.sbc.com>
2014-07-23 14:52:27 -0400

    Merge master into HEAD
    Conflicts:
    	shortener/BraveHackers/src/java/com/att/bravehackers/redirect/entity/service/ApplicationConfig.java
    	shortener/BraveHackers/web/index.xhtml

--- shortener/BraveHackers/src/java/com/att/bravehackers/redirect/entity/service/ApplicationConfig.java
+++ shortener/BraveHackers/src/java/com/att/bravehackers/redirect/entity/service/ApplicationConfig.java
@@ -31,6 +31,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(com.att.bravehackers.clicks.entity.service.ClicksFacadeREST.class);
+        resources.add(com.att.bravehackers.redirect.entity.service.UrlListFacadeREST.class);
     }
     
 }
