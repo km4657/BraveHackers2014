07a0c15c1766e7ebff68a4c75ea92e2ada32854e
ML5174 <ML5174@GACDTL03ML5174.ITServices.sbc.com>
2014-07-23 00:01:10 -0400

    redirect servlet

---
+++ shortener/BraveHackers/src/java/com/att/bravehackers/redirect/entity/service/UrlListFacadeREST.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.att.bravehackers.redirect.entity.service;
+
+import com.att.bravehackers.redirect.entity.UrlList;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author ML5174
+ */
+@Stateless
+@Path("com.att.bravehackers.redirect.entity.urllist")
+public class UrlListFacadeREST extends AbstractFacade<UrlList> {
+    @PersistenceContext(unitName = "BraveHackersPU")
+    private EntityManager em;
+
+    public UrlListFacadeREST() {
+        super(UrlList.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(UrlList entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") BigDecimal id, UrlList entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") BigDecimal id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public UrlList find(@PathParam("id") BigDecimal id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<UrlList> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<UrlList> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
