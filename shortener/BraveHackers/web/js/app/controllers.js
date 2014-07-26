65fb02fec52626778dcc1f534a7db4cdc5f4c51d
KM4657 <KM4657@GACDTL201KM4657.ITServices.sbc.com>
2014-07-26 14:35:57 -0400

    shorturl

--- shortener/BraveHackers/web/js/app/controllers.js
+++ shortener/BraveHackers/web/js/app/controllers.js
@@ -4,22 +4,24 @@
 
 angular.module('myApp.controllers', ['ngResource'])
         .controller('HomeCtrl', function($scope, $resource) {
-            $resource('http://gacdtl03ml5174.itservices.sbc.com:8080/r/webresources/com.att.bravehackers.redirect.urllist', {}, {
+            $resource('http://localhost:8080/r/webresources/com.att.bravehackers.redirect.urllist', {}, {
                 get: {method: "GET", isArray: true}
             }).get(function(t) {
                 $scope.urls = t;
             });
         })
-        .controller('CreateCtrl', ['$scope', function($scope) {
-                $scope.url = {category: "test", email: "weolopez@yahoo.com", expiredurl: "http://localhost:8080/r/faces/jsf/urlList/List.xhtml", longurl: "http://localhost:8080/r/faces/jsf/urlList/List.xhtml", shorturl: "urlList", urlName: "test1", vanityurl: "urlList"};
-                $scope.getURL = function() {
-                    $resource('http://gacdtl03ml5174.itservices.sbc.com:8080/r/webresources/com.att.bravehackers.redirect.urllist', {}, {
+        .controller('CreateCtrl', function($scope,$http) {
+            $scope.url = {category: "test", email: "weolopez@yahoo.com", expiredurl: "http://localhost:8080/r/faces/jsf/urlList/List.xhtml", longurl: "http://localhost:8080/r/faces/jsf/urlList/List.xhtml", shorturl: "urlList", urlName: "test1", vanityurl: "urlList"};
+            $scope.url2 = new Object();
+            $scope.url2.urlName = "testing again";
+            $scope.getURL = function() {
+                    $resource('http://localhost:8080/r/webresources/com.att.bravehackers.redirect.urllist',  {}, {
                         post: {method: "POST"}
                     }).post(function(t) {
                         $scope.urls = t;
                     });
-                }
-            }])
+                };
+            })
         .controller('HistoryCtrl', ['$scope', function($scope) {
 
             }])
